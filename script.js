3f61f416e70b56972c11a7cb6ac866b5429c6ac7
Tim_van_Gemert <EMAIL_ADDRESS>
2022-09-06 16:30:20 +0200

    added first version off darkmode

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// question1 = document.getElementById('question1');
+// question2 = document.getElementById('question2');
+// question3 = document.getElementById('question3');
+// question4 = document.getElementById('question4');
+// question5 = document.getElementById('question5');
+// question6 = document.getElementById('question6');
+
+
 
 function button_question_one() {
     document.getElementById('question1').style.display = 'none';
     document.getElementById('question2').style.display = 'flex';
 
     document.getElementById('button-1').style.display = 'none';
+    document.getElementById('button-1-1').style.display = 'none';
     document.getElementById('button-2').style.display = 'flex';
 
     document.getElementById('question-1-text').style.display = 'none';
@@ -82,4 +91,31 @@ function reset_animation() {
     el.style.animation = null; 
 }
 
+function Darkmode(){
 
+
+    const ButtonCollection = document.getElementsByClassName("button");
+    ButtonCollection[0].style.backgroundColor = "#3E3C3E";
+    ButtonCollection[0].style.color = "white";
+
+    ButtonCollection[1].style.backgroundColor = "#3E3C3E";
+    ButtonCollection[1].style.color = "white";
+
+    const QuestionCollection = document.getElementsByClassName("duck-text");
+    QuestionCollection[0].style.backgroundColor = "#3E3C3E";
+    QuestionCollection[0].style.color = "white";
+
+    const FieldCollection = document.getElementsByClassName("input-fields");
+    FieldCollection[0].style.backgroundColor = "#3E3C3E";
+    FieldCollection[0].style.color = "white";
+
+    document.getElementById('textcite').style.backgroundColor = '#3E3C3E';
+    // document.getElementById('main-right').style.backgroundColor = 'rgb(22, 40, 42)';
+
+
+
+    document.getElementById('main-left').style.backgroundColor = '#2A282A';
+    document.getElementById('intro').style.color = 'white';
+
+
+}
